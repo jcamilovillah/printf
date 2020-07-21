0c44876cae91940ec839a559301a29ef8b81bd92
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-21 21:33:48 +0000

    file header

---
+++ holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * struct s_type - Structure
+ * @args: pointer arguments
+ * @func: pointer function
+ */
+typedef struct s_type
+{
+	char *args;
+	int (*func)(va_list);
+} s_type;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int (*_typefor(const char *argu, int argb))(va_list);
+int p_char(va_list valist);
+int p_string(va_list valist);
+
+#endif
