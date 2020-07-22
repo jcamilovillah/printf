cb85ab1fc32ea0509c735e560faaa3dd250354fb
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-22 01:23:51 +0000

    add func print nums

--- _functions.c
+++ _functions.c
@@ -33,3 +33,54 @@ int p_string(va_list valist)
 	}
 	return (i);
 }
+
+/**
+ * count_num - print string.
+ * @n: argument
+ * Return: Void
+ */
+int count_num(unsigned int n)
+{
+	int count = 0;
+	unsigned int x, z;
+
+	if (n != 0)
+	{
+		z = (n / 10);
+		x = (n % 10);
+		count += count_num(z);
+		count++;
+		_putchar(x + '0');
+		return (count);
+	}
+	return (0);
+}
+/**
+ * p_int - print integer.
+ * @valist: argument
+ * Return: Void
+ */
+int p_int(va_list valist)
+{
+	int a, b = 0;
+	unsigned int x;
+
+	a = va_arg(valist, int);
+
+	if (a < 0)
+	{
+		_putchar('-');
+		b = 1;
+		x = a * (-1);
+	}
+	else
+	{
+		x = a;
+	}
+	if (x > 9)
+	{
+		return (b + count_num(x));
+	}
+	_putchar (x + '0');
+	return (1 + b);
+}
